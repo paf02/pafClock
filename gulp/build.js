de77d811788763ec339d0b8e02c8d62e6ea777ee
Pablo Montero <pablom@criticalmass.com>
2015-10-08 14:21:23 -0600

    fix in gh-pages

--- gulp/build.js
+++ gulp/build.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
+var ghPages = require('gulp-gh-pages');
 
 var $ = require('gulp-load-plugins')({
   pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
